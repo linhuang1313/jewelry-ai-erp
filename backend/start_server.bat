40c03b712be5a8a5ecdb52b3d2a3d850918fa8e2
wwh <1776569851@qq.com>
2026-02-05 13:38:46 +0800

    update python venv

--- backend/start_server.bat
+++ backend/start_server.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting backend server with Python 3.10...
-py -3.10 -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+.venv\scripts\python -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
 pause
 
 
